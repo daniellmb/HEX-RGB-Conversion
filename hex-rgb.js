ec2d1ae25d21aba43ed61e872652d53a0dbb2942
Daniel Lamb <daniellmb@hotmail.com>
2011-07-29 12:09:49 -0700

    recompiled source

--- hex-rgb.js
+++ hex-rgb.js
@@ -1,2 +1,1 @@
-(function(a){a.toRGB=function(a){var b=parseInt(a,16);return[b>>16,b>>8&255,b&255]},a.toHex=function(a,b,c){var d=(c|b<<8|a<<16).toString(16);return"000000".substr(0,6-d.length)+d}})(this)
-
+(function(b){b.toRGB=function(a){a=parseInt(a,16);return[a>>16,a>>8&255,a&255]};b.toHex=function(a,b,c){a=(c|b<<8|a<<16).toString(16);return"000000".substr(0,6-a.length)+a}})(this);
